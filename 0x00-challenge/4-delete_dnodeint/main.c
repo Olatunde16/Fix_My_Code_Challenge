758d335b8f992fd899d67d7dcac241ba9e6e7a2b
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-02-13 11:32:59 +0100

    Update main.c

--- 0x00-challenge/4-delete_dnodeint/main.c
+++ 0x00-challenge/4-delete_dnodeint/main.c
@@ -1,3 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
 int main(void)
 {
     dlistint_t *head = NULL;
