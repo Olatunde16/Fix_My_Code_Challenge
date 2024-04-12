285b37bf3acdbac4f5015bd6a9f52c367d318013
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-04-12 08:55:07 +0100

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
 
-class Square:
-    
+class Square():
+    """ Square class."""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Instantiation of class."""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def perimeter_of_my_square(self):
-        """ Perimeter of the square """
-        return self.width * 4
+    def permiter_of_my_square(self):
+        """ Perimeter of the square."""
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Printable representation of square object."""
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
-    s = Square(width=12, height=12)  
+    """ Creates Square object."""
+    s = Square(width=12, height=9)
     print(s)
-    print("Area:", s.area_of_my_square())
-    print("Perimeter:", s.perimeter_of_my_square())
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
