bc1258f43ace9b440a554a33a24a51d359d46cd8
Olutunmibi Iseoluwa <olatundeolutunmibi@gmail.com>
2024-04-12 00:50:19 -0700

    Ongoing

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+class Square:
+    
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def perimeter_of_my_square(self):
+        """ Perimeter of the square """
+        return self.width * 4
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+    s = Square(width=12, height=12)  
+    print(s)
+    print("Area:", s.area_of_my_square())
+    print("Perimeter:", s.perimeter_of_my_square())
